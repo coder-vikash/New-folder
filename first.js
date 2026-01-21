2000b21aeef4f5627bad0b529d4539b7789dcb67
coder-vikash <vk422821@gmail.com>
2026-01-21 17:28:19 +0530

    ss

--- first.js
+++ first.js
@@ -1,3 +1,34 @@
+// Example of Promises in JavaScript to avoid callback hell
+function AsyncFun1() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("Async Function 1 Complete");
+      resolve("success");
+    }, 2000);
+  });
+}
+
+function AsyncFun2() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("Async Function 2 Complete");
+      resolve("success");
+    }, 4000);
+  });
+}
+
+console.log("Starting Async Function 1");
+let p1 = AsyncFun1();
+p1.then((res) => {
+  console.log("Promise 1 Resolved:", res);
+  console.log("Starting Async Function 2");
+  let p2 = AsyncFun2();
+  p2.then((res) => {
+    console.log("Promise 2 Resolved:", res);
+  });
+});
+
+/*
 //This is an example of callback hell in JavaScript
 function getData(dataId, nextDataid) {
   setTimeout(() => {
@@ -14,3 +45,4 @@ getData(1, () => {
     });
   });
 });
+*/
